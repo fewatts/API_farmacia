aeaa642579bd6c6ec01ed31d3b5bc7b07489c8ab
Fewatts <feralveswatts@gmail.com>
2023-03-20 11:02:24 -0300

    Método getAll ok

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,55 @@
+package com.generation.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome do produto é obrigatório")
+    @Size(min = 5, max = 100, message = "O nome deve conter no mínimo 5 e no máximo 100 caracteres.")
+    private String nome;
+
+    @Positive
+    private Integer quantidade;
+
+    @Positive
+    @Digits(integer = 6, fraction = 2, message = "O preço é obrigatório!")
+    private Float valor;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Float getValor() {
+        return valor;
+    }
+
+    public void setValor(Float valor) {
+        this.valor = valor;
+    } 
+    
+}
